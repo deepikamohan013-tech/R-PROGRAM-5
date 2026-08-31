36b69fa3f16aa53969fd949693f0807f80a5c0da
deepikamohan013-tech <deepikamohan013@gmail.com>
2026-08-31 12:54:11 +0530

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,14 @@
+DROP DATABASE IF EXISTS CollegeDB;
+CREATE DATABASE CollegeDB;
+USE CollegeDB;
 
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    Gender VARCHAR(10),
+    DepartmentID INT
+);
+desc Student;
+
+insert Student values("1001","Arun","Male","101"),("1002","Divya","Female","102"),("1003","Karthik","Male","101");
+select * from Student;
